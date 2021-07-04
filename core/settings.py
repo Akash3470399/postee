51cd359df98339186652c43e61401fe35ab38378
Akash Bhagat <bhagatakash0123@gmail.com>
2021-07-04 14:21:30 +0530

    Configured database

--- core/settings.py
+++ core/settings.py
@@ -102,16 +102,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # }
 
 
-DATABASE = {
-    'default':{
-        'ENGINE':'django.db.backend.sqlite3',
-        'NAME':BASE_DIR/'db.sqlite3',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
